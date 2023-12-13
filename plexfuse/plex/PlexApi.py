10a8b0120dfa5bc0674b053ab6315633ce99e353
Elan Ruusamäe <glen@pld-linux.org>
2023-12-13 21:46:45 +0200

    Add section_types property to PlexApi

--- plexfuse/plex/PlexApi.py
+++ plexfuse/plex/PlexApi.py
@@ -7,3 +7,15 @@ class PlexApi:
     @cached_property
     def server(self):
         return PlexServer()
+
+    @property
+    def library(self):
+        return self.server.library
+
+    @cached_property
+    def sections(self):
+        return self.library.sections()
+
+    @cached_property
+    def section_types(self):
+        return {s.type for s in self.sections}
