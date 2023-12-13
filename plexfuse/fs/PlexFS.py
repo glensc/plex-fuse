57562f6c3c6d1c9157834a16644732a04d6ac13c
Elan Ruusamäe <glen@pld-linux.org>
2023-12-13 22:20:27 +0200

    Add listing of section types

--- plexfuse/fs/PlexFS.py
+++ plexfuse/fs/PlexFS.py
@@ -14,11 +14,14 @@ class PlexFS(fuse.Fuse):
 
     def getattr(self, path: str):
         st = PlexDirectory()
+        pe = path.split("/")[1:]
 
         st.st_atime = int(time())
         st.st_mtime = st.st_atime
         st.st_ctime = st.st_atime
         if path == "/":
+            pass
+        elif pe[-1] in self.plex.section_types:
             pass
         else:
             return -errno.ENOENT
@@ -26,5 +29,8 @@ class PlexFS(fuse.Fuse):
 
     def readdir(self, path: str, offset: int):
         dirents = [".", ".."]
+        if path == "/":
+            dirents.extend(self.plex.section_types)
+
         for r in dirents:
             yield fuse.Direntry(r)
