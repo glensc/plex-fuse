82fc8b0d2147e896c92b66211ab6fdde3e942fc2
Elan Ruusamäe <glen@pld-linux.org>
2023-12-13 21:59:40 +0200

    Implement root directory listing

--- plexfuse/fs/PlexFS.py
+++ plexfuse/fs/PlexFS.py
@@ -1,5 +1,25 @@
+import errno
+from time import time
+
 import fuse
+
+from plexfuse.fs.PlexDirectory import PlexDirectory
 
 
 class PlexFS(fuse.Fuse):
-    ...
+    def getattr(self, path: str):
+        st = PlexDirectory()
+
+        st.st_atime = int(time())
+        st.st_mtime = st.st_atime
+        st.st_ctime = st.st_atime
+        if path == "/":
+            pass
+        else:
+            return -errno.ENOENT
+        return st
+
+    def readdir(self, path: str, offset: int):
+        dirents = [".", ".."]
+        for r in dirents:
+            yield fuse.Direntry(r)
