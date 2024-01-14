bfb8f8fc9cc4d7a043a6091102387530786bed0d
Elan Ruusamäe <glen@pld-linux.org>
2024-01-15 01:07:04 +0200

    Add typing to PlexApi return values

--- plexfuse/plex/PlexApi.py
+++ plexfuse/plex/PlexApi.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
+
 from functools import cached_property
+from typing import TYPE_CHECKING
 
 from plexapi.server import PlexServer
+
+if TYPE_CHECKING:
+    from plexapi.library import (MovieSection, MusicSection, PhotoSection,
+                                 ShowSection)
+
+    SectionTypes = MovieSection | ShowSection | MusicSection | PhotoSection
 
 
 class PlexApi:
@@ -13,12 +22,12 @@ class PlexApi:
         return self.server.library
 
     @cached_property
-    def sections(self):
+    def sections(self) -> list[SectionTypes]:
         return self.library.sections()
 
     @property
-    def section_types(self):
+    def section_types(self) -> set[str]:
         return {s.type for s in self.sections}
 
-    def sections_by_type(self, type: str):
+    def sections_by_type(self, type: str) -> set[str]:
         return {s.title for s in self.sections if s.type == type}
