04518ccea5dce24ed725934e99511c16b6a0c841
Elan Ruusamäe <glen@pld-linux.org>
2024-01-15 05:28:43 +0200

    Refactor getattr to use vfs logic

--- plexfuse/fs/PlexFS.py
+++ plexfuse/fs/PlexFS.py
@@ -17,25 +17,21 @@ class PlexFS(fuse.Fuse):
     def getattr(self, path: str):
         st = PlexDirectory()
 
-        if path == "/":
-            st.st_nlink = 2 + len(self.vfs[path])
-            return st
+        try:
+            item = self.vfs[path]
+        except ValueError as e:
+            print(e)
+            return -errno.ENOENT
 
         pe = path.split("/")[1:]
         pc = len(pe)
 
-        if pc == 1 and pe[0] in self.plex.section_types:
-            st.st_nlink = 2 + len(self.vfs[path])
-        elif pc == 2 and pe[1] in self.plex.sections_by_type(pe[0]):
-            st.st_nlink = 2 + len(self.vfs[path])
-        elif pc == 3 and pe[2] in self.plex.library_items_titles(pe[1]):
-            st.st_nlink = 2 + len(self.vfs[path])
-        elif pc == 4 and pe[0] == "movie":
-            part = self.vfs[path][0]
-
+        if pc == 4 and pe[0] == "movie":
+            part = item[0]
             return PlexFile(st_size=part.size)
         else:
-            return -errno.ENOENT
+            st.st_nlink = 2 + len(item)
+
         return st
 
     def readdir(self, path: str, offset: int):
