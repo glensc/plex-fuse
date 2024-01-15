b5f1f780cf757eb72aae574ed8ad2347e25072dc
Elan Ruusamäe <glen@pld-linux.org>
2024-01-15 13:59:06 +0200

    Cleanup st_nlink force for PlexDirectory

--- plexfuse/fs/PlexDirectory.py
+++ plexfuse/fs/PlexDirectory.py
@@ -8,7 +8,6 @@ class PlexDirectory(Stat):
     def __init__(self, **kw):
         super().__init__(**kw)
         self.st_mode = stat.S_IFDIR | 0o755
-        self.st_nlink = 2
         self.st_atime = int(time())
         self.st_mtime = self.st_atime
         self.st_ctime = self.st_atime
