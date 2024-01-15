0df793dc983e986cd4d2916d5053e37aa1a7e3f2
Elan Ruusamäe <glen@pld-linux.org>
2024-01-15 02:07:48 +0200

    Implement PlexFile type

--- plexfuse/fs/PlexFile.py
+++ plexfuse/fs/PlexFile.py
@@ -1,5 +1,14 @@
+import stat
+from time import time
+
 from fuse import Stat
 
 
 class PlexFile(Stat):
-    ...
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.st_mode = stat.S_IFREG | 0o644
+        self.st_nlink = 1
+        self.st_atime = int(time())
+        self.st_mtime = self.st_atime
+        self.st_ctime = self.st_atime
