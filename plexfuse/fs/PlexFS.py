063ada2d8c12cdd1fc6652e3c0c45e92d5316cb9
Elan Ruusamäe <glen@pld-linux.org>
2024-01-15 02:11:27 +0200

    Implement listing of movie files

--- plexfuse/fs/PlexFS.py
+++ plexfuse/fs/PlexFS.py
@@ -3,6 +3,7 @@ import errno
 import fuse
 
 from plexfuse.fs.PlexDirectory import PlexDirectory
+from plexfuse.fs.PlexFile import PlexFile
 from plexfuse.plex.PlexApi import PlexApi
 
 
@@ -24,6 +25,11 @@ class PlexFS(fuse.Fuse):
             pass
         elif pc == 3 and pe[2] in self.plex.library_items_titles(pe[1]):
             pass
+        elif pc == 4 and pe[0] == "movie" \
+                and (m := self.plex.library_item(pe[1], pe[2])) \
+                and pe[3] in self.plex.media_parts(m):
+
+            return PlexFile()
         else:
             return -errno.ENOENT
         return st
@@ -39,6 +45,10 @@ class PlexFS(fuse.Fuse):
             dirents.extend(self.plex.sections_by_type(pe[0]))
         elif pc == 2:
             dirents.extend(self.plex.library_items_titles(pe[1]))
+        elif pc == 3 and pe[0] == "movie":
+            item = self.plex.library_item(pe[1], pe[2])
+            parts = self.plex.media_parts(item)
+            dirents.extend(parts)
 
         for r in dirents:
             yield fuse.Direntry(r)
