1733feb73b245f52109560df8f56aaf4eb83c7d4
Elan Ruusamäe <glen@pld-linux.org>
2024-01-21 00:47:43 +0200

    Implement file reading using download cache

--- plexfuse/fs/PlexFS.py
+++ plexfuse/fs/PlexFS.py
@@ -1,10 +1,13 @@
 import errno
+import os
 from functools import cache
 
 import fuse
 
 from plexfuse.fs.PlexDirectory import PlexDirectory
 from plexfuse.fs.PlexFile import PlexFile
+from plexfuse.plex.DownloadCache import DownloadCache
+from plexfuse.plex.FileCache import FileCache
 from plexfuse.plex.PlexApi import PlexApi
 from plexfuse.plex.PlexVFS import PlexVFS
 from plexfuse.plex.PlexVFSFileEntry import PlexVFSFileEntry
@@ -13,7 +16,11 @@ from plexfuse.plex.PlexVFSFileEntry import PlexVFSFileEntry
 class PlexFS(fuse.Fuse):
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
-        self.vfs = PlexVFS(PlexApi())
+        plex = PlexApi()
+        self.vfs = PlexVFS(plex)
+        self.cache = DownloadCache(plex)
+        self.cache_map = {}
+        self.files = FileCache()
 
     @cache
     def getattr(self, path: str):
@@ -44,3 +51,47 @@ class PlexFS(fuse.Fuse):
 
         for r in it:
             yield fuse.Direntry(str(r))
+
+    def read(self, path, size, offset):
+        try:
+            cache_path = self.cache_map[path]
+            with self.files[cache_path] as fh:
+                fh.seek(offset, os.SEEK_SET)
+                return fh.read(size)
+        except KeyError:
+            return -errno.EINVAL
+
+    def release(self, path, flags):
+        try:
+            cache_path = self.cache_map[path]
+            self.files.release(cache_path)
+        except KeyError as e:
+            print(f"release({path}): {e}")
+            return -errno.EINVAL
+
+        return 0
+
+    def open(self, path, flags):
+        try:
+            part = self.vfs[path]
+        except KeyError as e:
+            print(f"open vfs({path}): {e}")
+            return -errno.ENOENT
+
+        if not isinstance(part, PlexVFSFileEntry):
+            return -errno.EISDIR
+
+        try:
+            cache_path = self.cache[part]
+        except KeyError as e:
+            print(f"open cache({path}): {e}")
+            self.cache[part] = None
+            return -errno.ENOMEM
+
+        if cache_path is None:
+            return -errno.ENOMEM
+
+        self.cache_map[path] = cache_path
+        self.files.open(cache_path)
+
+        return 0
