d073ee92ca28b4835db5c9e0797e260a8295a17e
Elan Ruusamäe <glen@pld-linux.org>
2024-01-21 20:38:07 +0200

    Remove open/close spam

--- plexfuse/plex/FileCache.py
+++ plexfuse/plex/FileCache.py
@@ -21,10 +21,8 @@ class FileCache(UserDict[str, BinaryIO]):
 
     def open(self, path: str):
         if path not in self:
-            print(f"open: {path}, refcount={self.nopen[path]}")
             self[path] = self._open(path)
             self.nopen[path] += 1
-            print(f"opened: {path}, refcount={self.nopen[path]}")
 
         return self[path]
 
@@ -34,11 +32,9 @@ class FileCache(UserDict[str, BinaryIO]):
 
         self.nopen[path] -= 1
         if self.nopen[path] <= 0:
-            print(f"close: {path}, refcount={self.nopen[path]}")
             fh = self[path]
             del self[path]
             fh.close()
-            print(f"closed: {path}, refcount={self.nopen[path]}")
 
     def _open(self, path: str):
         try:
