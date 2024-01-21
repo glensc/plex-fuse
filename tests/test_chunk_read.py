57ee50131ad43c76fdb31a0e94b2cdaad5c12538
Elan Ruusamäe <glen@pld-linux.org>
2024-01-21 12:19:45 +0200

    Add test verifying actual read

--- tests/test_chunk_read.py
+++ tests/test_chunk_read.py
@@ -14,3 +14,12 @@ def test_file():
     assert len(cf.read(key, 0, cf.chunk_size)) == cf.chunk_size
     # A test reading chunk that spans two chunk
     assert len(cf.read(key, int(cf.chunk_size / 2), cf.chunk_size)) == cf.chunk_size
+
+    # A test that chunked content is full content
+    cf.chunk_size = 6
+    offset = 8
+    size = 8
+    chunked = cf.read(key, offset, size)
+    cf.chunk_size = 1024
+    unchunked = cf.read(key, offset, size)
+    assert chunked == unchunked
