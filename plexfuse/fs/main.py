fe434a959123a36ff4edbbe7ea70fea5a58fe92c
Elan Ruusamäe <glen@pld-linux.org>
2024-01-21 03:43:35 +0200

    Add -o cache_path option

--- plexfuse/fs/main.py
+++ plexfuse/fs/main.py
@@ -10,6 +10,7 @@
 import fuse
 
 from plexfuse.fs.PlexFS import PlexFS
+from plexfuse.plex.PlexApi import PlexApi
 
 fuse.fuse_python_api = (0, 2)
 
@@ -18,7 +19,10 @@ def main():
     usage = "PlexFS: A filesystem for mounting a Plex Media Server media\n\n"
     server = PlexFS(version="%prog " + fuse.__version__,
                     usage=usage + fuse.Fuse.fusage, dash_s_do="setsingle")
-    server.parse(errex=1)
+    server.parser.add_option(mountopt="cache_path", metavar="PATH",
+                             default=PlexApi.CACHE_PATH,
+                             help="cache downloads under PATH [default: %default]")
+    server.parse(values=server, errex=1)
     server.main()
 
 
