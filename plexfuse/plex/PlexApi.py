1a139ecb16fe41f6f7fd0d8cda3db3a942909221
Elan Ruusamäe <glen@pld-linux.org>
2024-01-21 05:13:35 +0200

    Add download_part support for offset, size

--- plexfuse/plex/PlexApi.py
+++ plexfuse/plex/PlexApi.py
@@ -147,11 +147,12 @@ class PlexApi:
 
         yield from response.iter_content(chunk_size=self.CHUNK_SIZE)
 
-    def download_part(self, key: str, savepath: Path, overwrite=False):
+    def download_part(self, key: str, savepath: Path,
+                      size=None, offset=None, overwrite=False):
         if overwrite is False and savepath.exists():
             return savepath
 
-        content = self.request_file(key)
+        content = self.request_file(key, size, offset)
         makedirs(savepath.parent, exist_ok=True)
         with open(savepath, "wb") as handle:
             for chunk in content:
