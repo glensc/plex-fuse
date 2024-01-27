9ff42ad4acb8fe28355a3a977ac23ac5a241d5b6
Elan Ruusamäe <glen@pld-linux.org>
2024-01-27 18:08:45 +0200

    Refactor PlexVFSEpisode title to include optional year and guid

--- plexfuse/plex/PlexVFSEpisode.py
+++ plexfuse/plex/PlexVFSEpisode.py
@@ -1,10 +1,25 @@
 from __future__ import annotations
 
+from functools import cached_property
+
 
 class PlexVFSEpisode:
     def __init__(self, item):
         self.item = item
-        self.title = item.title
 
     def __str__(self):
         return self.title
+
+    @cached_property
+    def title(self):
+        # Handle directory separator in filename
+        title = self.item.title.replace("/", "∕")
+        year = self.item.__dict__.get("year", None)
+        if year:
+            title += f" ({year})"
+
+        guids = self.item.guids if self.item.guid.startswith("plex://") else []
+        for guid in guids:
+            title += f" {{{guid.id.replace('://', '-')}}}"
+
+        return title
