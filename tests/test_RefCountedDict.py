02ad2510aa6ef74c5b8c0564a9ca668615708230
Elan Ruusamäe <glen@pld-linux.org>
2024-01-27 13:11:15 +0200

    Add test for wrong value assign

--- tests/test_RefCountedDict.py
+++ tests/test_RefCountedDict.py
@@ -12,6 +12,12 @@ def test_file():
     assert "key1" in d, "set value again with same value increases refcount"
     assert d["key1"] == "value1"
 
+    try:
+        d["key1"] = "value2"
+        assert False, "setting different value must throw KeyError"
+    except ValueError as e:
+        assert str(e) == 'Value value2 already exists for key1', "Unexpected ValueError"
+
     del d["key1"]
     assert "key1" in d, "deleting will keep last value if refcount>0"
     assert d["key1"] == "value1"
