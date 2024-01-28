f76fd9478ba544088301889e850ec607715b12f7
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 19:41:56 +0200

    Add items property to SectionEntry

--- plexfuse/plexvfs/SectionEntry.py
+++ plexfuse/plexvfs/SectionEntry.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from functools import cached_property
 from typing import TYPE_CHECKING
+
+from plexfuse.plexvfs.MovieEntry import MovieEntry
 
 if TYPE_CHECKING:
     from plexfuse.plex.types import SectionTypes
@@ -8,12 +11,17 @@ if TYPE_CHECKING:
 
 class SectionEntry:
     def __init__(self, section: SectionTypes):
+        self.section = section
         self.title: str = section.title
         self.type: str = section.type
 
     @property
     def name(self):
         return self.title
+
+    @cached_property
+    def items(self):
+        return [MovieEntry(m) for m in self.section.search()]
 
     def __str__(self):
         return self.name
