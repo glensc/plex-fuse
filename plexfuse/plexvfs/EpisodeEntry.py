09c98b3061a5bf52e20b7ceb1bab357dcc2a6057
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 15:16:42 +0200

    Update EpisodeEntry to use Playable

--- plexfuse/plexvfs/EpisodeEntry.py
+++ plexfuse/plexvfs/EpisodeEntry.py
@@ -1,30 +1,13 @@
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING
+
+from plexfuse.plexvfs.Playable import Playable
 
 if TYPE_CHECKING:
     from plexapi.video import Episode
 
 
-class EpisodeEntry:
+class EpisodeEntry(Playable):
     def __init__(self, item: Episode):
-        self.item = item
-
-    def __str__(self):
-        return self.title
-
-    @cached_property
-    def title(self):
-        title = f"{self.item.seasonEpisode} "
-        # Handle directory separator in filename
-        title += self.item.title.replace("/", "∕")
-        year = self.item.__dict__.get("year", None)
-        if year:
-            title += f" ({year})"
-
-        guids = self.item.guids if self.item.guid.startswith("plex://") else []
-        for guid in guids:
-            title += f" {{{guid.id.replace('://', '-')}}}"
-
-        return title
+        super().__init__(item)
