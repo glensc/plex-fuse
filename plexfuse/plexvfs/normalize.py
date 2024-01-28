34d7fb3154267e1d512b0382bd64bfff8cc9b96c
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 21:12:45 +0200

    Add is_path check to normalize

--- plexfuse/plexvfs/normalize.py
+++ plexfuse/plexvfs/normalize.py
@@ -1,9 +1,10 @@
 import unicodedata
 
 
-def normalize(s: str):
-    # Handle directory separator in filename
-    s = s.replace("/", "∕")
+def normalize(s: str, is_path=False):
+    if not is_path:
+        # Handle directory separator in filename
+        s = s.replace("/", "∕")
     # Normalize Unicode
     # https://stackoverflow.com/questions/16467479/normalizing-unicode
     return unicodedata.normalize("NFC", s)
