685d1a59bfd819880ce9ecec8d0cf8f5a505ec7b
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 19:51:05 +0200

    Add seasons and episodes property to SectionEntry

--- plexfuse/plexvfs/SectionEntry.py
+++ plexfuse/plexvfs/SectionEntry.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from functools import cached_property
 from typing import TYPE_CHECKING
 
+from plexfuse.plexvfs.EpisodeEntry import EpisodeEntry
 from plexfuse.plexvfs.MovieEntry import MovieEntry
+from plexfuse.plexvfs.SeasonEntry import SeasonEntry
 
 if TYPE_CHECKING:
     from plexfuse.plex.types import SectionTypes
@@ -26,6 +28,14 @@ class SectionEntry:
     @cached_property
     def items_by_title(self):
         return dict({(m.title, m) for m in self.items})
+
+    @cached_property
+    def seasons(self):
+        return [SeasonEntry(s) for s in self.section.search(libtype="season")]
+
+    @cached_property
+    def episodes(self):
+        return [EpisodeEntry(m) for m in self.section.search(libtype="episode")]
 
     def __str__(self):
         return self.name
