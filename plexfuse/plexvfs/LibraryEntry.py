6366b878d9222eb328534a0345707d75fe66ecf9
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 19:18:47 +0200

    Add sections property LibraryEntry

--- plexfuse/plexvfs/LibraryEntry.py
+++ plexfuse/plexvfs/LibraryEntry.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from functools import cached_property
 from typing import TYPE_CHECKING
+
+from plexfuse.plexvfs.SectionEntry import SectionEntry
 
 if TYPE_CHECKING:
     from plexapi.library import Library
@@ -9,3 +12,7 @@ if TYPE_CHECKING:
 class LibraryEntry:
     def __init__(self, library: Library):
         self.library = library
+
+    @cached_property
+    def sections(self):
+        return [SectionEntry(s) for s in self.library.sections()]
