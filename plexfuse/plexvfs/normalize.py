0c3b73ccfcc0152cef4bdb85e579058f7312e282
Elan Ruusamäe <glen@pld-linux.org>
2024-01-28 20:39:42 +0200

    Replace ASCII path separator in normalize

--- plexfuse/plexvfs/normalize.py
+++ plexfuse/plexvfs/normalize.py
@@ -2,6 +2,8 @@ import unicodedata
 
 
 def normalize(s: str):
+    # Handle directory separator in filename
+    s = s.replace("/", "∕")
     # Normalize Unicode
     # https://stackoverflow.com/questions/16467479/normalizing-unicode
     return unicodedata.normalize("NFC", s)
