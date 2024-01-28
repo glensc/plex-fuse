37b17d693ca4ed13fdc9070b9092bbb7d1f1056b
Elan Ruusamäe <glen@pld-linux.org>
2024-01-29 00:57:00 +0200

    Update PlexFile, PlexDirectory to extend from Timestampable

--- plexfuse/fs/PlexDirectory.py
+++ plexfuse/fs/PlexDirectory.py
@@ -1,13 +1,12 @@
 import stat
-from time import time
 
 from fuse import Stat
 
+from plexfuse.fs.Timestampable import Timestampable
 
-class PlexDirectory(Stat):
+
+class PlexDirectory(Stat, Timestampable):
     def __init__(self, **kw):
         super().__init__(**kw)
+        Timestampable.__init__(self)
         self.st_mode = stat.S_IFDIR | 0o755
-        self.st_atime = int(time())
-        self.st_mtime = self.st_atime
-        self.st_ctime = self.st_atime
