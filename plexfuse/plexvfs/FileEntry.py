7ca4e2440d54f1954bef873d8c009093bb38bf39
Elan Ruusamäe <glen@pld-linux.org>
2024-01-29 01:13:33 +0200

    Add timestamps to FileEntry via playable arg

--- plexfuse/plexvfs/FileEntry.py
+++ plexfuse/plexvfs/FileEntry.py
@@ -5,11 +5,20 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from plexapi.media import MediaPart
 
+    from plexfuse.plexvfs.Playable import Playable
+
 
 class FileEntry:
-    def __init__(self, part: MediaPart):
+    def __init__(self, part: MediaPart, playable: Playable = None):
         self.size = part.size
         self.key = part.key
+        self.playable = playable
+
+    def timestamps(self):
+        try:
+            return self.playable.timestamps
+        except AttributeError:
+            return {}
 
     def __len__(self):
         return self.size
