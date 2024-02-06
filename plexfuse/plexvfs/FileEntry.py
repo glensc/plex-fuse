b1789b7ef40181a14db0aa7bf96ca3a390b3f195
Elan Ruusamäe <glen@pld-linux.org>
2024-02-06 12:09:32 +0200

    Add read() support to FileEntry

--- plexfuse/plexvfs/FileEntry.py
+++ plexfuse/plexvfs/FileEntry.py
@@ -9,16 +9,20 @@ if TYPE_CHECKING:
 
 
 class FileEntry:
-    def __init__(self, part: MediaPart, playable: Playable = None):
+    def __init__(self, part: MediaPart, playable: Playable = None, reader=None):
         self.size = part.size
         self.key = part.key
         self.playable = playable
+        self.reader = reader
 
     def timestamps(self):
         try:
             return self.playable.timestamps
         except AttributeError:
             return {}
+
+    def read(self, offset: int, size: int):
+        return self.reader.read(self.key, size=size, offset=offset, max_size=self.size)
 
     def __len__(self):
         return self.size
