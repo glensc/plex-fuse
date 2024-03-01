9eb39b9d0b743888824bb7bfb92f619fec348eec
Elan Ruusamäe <glen@pld-linux.org>
2024-03-01 21:04:50 +0200

    Add handle method to Control

--- plexfuse/plexvfs/Control.py
+++ plexfuse/plexvfs/Control.py
@@ -1,3 +1,7 @@
+from plexfuse.plexvfs.ControlEntry import ControlEntry
+from plexfuse.plexvfs.DirEntry import DirEntry
+
+
 class Control:
     @property
     def root(self):
@@ -6,3 +10,13 @@ class Control:
     @property
     def commands(self):
         return ["reload", "status"]
+
+    def handle(self, pc: int, pe: list[str]):
+        if pc == 1 and pe[0] in self.root:
+            return DirEntry(self.commands)
+
+        if pc != 2 or pe[0] != self.root[0]:
+            return
+
+        if pe[1] in self.commands:
+            return ControlEntry(pe[1])
