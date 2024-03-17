b1688cccc1d411363f3b4d5adacbe4604a7a02ef
Elan Ruusamäe <glen@pld-linux.org>
2024-03-18 00:59:57 +0200

    Use get_size to set size

--- plexfuse/vfs/entry/FileEntry.py
+++ plexfuse/vfs/entry/FileEntry.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 
 class FileEntry(AttrEntry):
     def __init__(self, part: MediaPart, playable: Playable, reader: ChunkedFile):
-        self.size = part.size if part.exists else None
+        self.size = self.get_size(playable, part)
         self.key = part.key
         self.playable = playable
         self.reader = reader
