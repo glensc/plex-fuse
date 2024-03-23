124a67be780897021d453dd8cf3539a112b0777e
Elan Ruusamäe <glen@pld-linux.org>
2024-03-23 14:15:56 +0200

    fixup! fixup! Add CacheCleanup class

--- plexfuse/cache/CacheCleanup.py
+++ plexfuse/cache/CacheCleanup.py
@@ -1,6 +1,15 @@
+import time
+from datetime import timedelta
+
+from timeloop import Timeloop
+
+tl = Timeloop()
+
+
 class CacheCleanup:
     def __init__(self):
         ...
 
+    @tl.job(interval=timedelta(seconds=5))
     def cleanup(self):
-        ...
+        print("run cleanup:", time.ctime())
