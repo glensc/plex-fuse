eaf0b3a3006740c60ed85cb9eeb013d1cd66b82b
Elan Ruusamäe <glen@pld-linux.org>
2024-04-10 16:47:20 +0300

    Create SymlinkEntry base class

---
+++ plexfuse/vfs/entry/SymlinkEntry.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import stat
+
+from plexfuse.vfs.entry.AttrEntry import AttrEntry
+
+
+class SymlinkEntry(AttrEntry):
+    @property
+    def size(self):
+        raise 0
+
+    @property
+    def link(self):
+        raise NotImplementedError("Must implement link property")
+
+    @property
+    def attr(self):
+        return {
+            "st_mode": stat.S_IFLNK | 0o644
+        }
