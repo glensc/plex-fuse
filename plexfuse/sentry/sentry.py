5a3c0f2e1ea565145363a26c2dd8a9a1311d670e
Elan Ruusamäe <glen@pld-linux.org>
2024-05-11 14:16:26 +0300

    Add capture_message, capture_exception to sentry module

--- plexfuse/sentry/sentry.py
+++ plexfuse/sentry/sentry.py
@@ -1,3 +1,6 @@
+sdk = None
+
+
 def sentry(dsn: str = None):
     if not dsn:
         return
@@ -7,3 +10,18 @@ def sentry(dsn: str = None):
     sentry_sdk.init(
         dsn=dsn,
     )
+
+    # this is a pointer to the module object instance itself.
+    import sys
+    module = sys.modules[__name__]
+    module.sdk = sentry_sdk
+
+
+def capture_message(*args, **kwargs):
+    if sdk:
+        sdk.capture_message(*args, **kwargs)
+
+
+def capture_exception(*args, **kwargs):
+    if sdk:
+        sdk.capture_exception(*args, **kwargs)
